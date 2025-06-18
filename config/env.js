e40ec0ca5eeecfa6e2d55ec0ac233bd086fce9af
mahmudulshawn <mahmudulalamshawn@gmail.com>
2025-06-18 19:39:07 +0600

    minor change in env.js

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+config({ path: `.env` });
 
 export const { PORT, NODE_ENV, GEMINI_API_KEY } = process.env;
